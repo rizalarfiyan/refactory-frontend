1605af8a951738a4cbaf67d7a97093b6f6d3f69f
rizalarfiyan <rizal.arfiyan.23@gmail.com>
2021-08-22 11:44:15 +0700

    feat: vue router

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,14 @@ module.exports = {
   env: {
     node: true,
     browser: true,
-    es2020: true
+    es2020: true,
   },
   extends: ['plugin:vue/essential', 'plugin:prettier/recommended', 'eslint:recommended'],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  }
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
 }
